77934495e825d2216286f83a531b73e9b3b0fab2
Mike Cann <mike.cann@gmail.com>
2026-02-18 15:25:43 +0800

    chore: remove leftover _do_commit.ps1 temp script

--- taskmon/_do_commit.ps1
+++ taskmon/_do_commit.ps1
@@ -1,5 +1,5 @@
 Set-Location "c:\dev\me\mikes-windows-tools"
-$msg = "taskmon: polished settings UI, fix ComboBox dropdown z-order, add video to README`n`nSettings dialog: light mode theme, colored metric indicator dots, blue accent`nApply button, section headers with separator lines, icon in title bar.`nFix: pause z-order timer while settings open so ComboBox dropdown stays on top.`nREADME: add vid1.mp4 demo video to taskmon section."
+$msg = "chore: remove leftover _do_commit.ps1 temp script"
 $tmp = [System.IO.Path]::GetTempFileName()
 [System.IO.File]::WriteAllText($tmp, $msg)
 git add -A
